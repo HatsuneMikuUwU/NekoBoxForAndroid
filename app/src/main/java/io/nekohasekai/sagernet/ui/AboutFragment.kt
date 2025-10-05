bc285f05ed58765091fb4dd51c983d732d69e5a7
Hatsune Miku <146099520+HatsuneMikuUwU@users.noreply.github.com>
2025-10-05 09:04:39 +0700

    Update AboutFragment.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/AboutFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/AboutFragment.kt
@@ -190,9 +190,7 @@ class AboutFragment : ToolbarFragment(R.layout.layout_about) {
                 .build()
 
                 }
-                            }
-                        }
-                        .build())
+              }
                 .addCard(
                     MaterialAboutCard.Builder()
                         .outline(false)
